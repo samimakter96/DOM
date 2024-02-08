9922ab9547b84b3cfd9e0162bae0250bf0c9dde8
samimakter96 <samimakter9641@gmail.com>
2024-02-08 15:23:56 +0530

    getElementById method

---
+++ getElementById-Method/script.js
@@ -0,0 +1,15 @@
+const mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fruit World";
+mainHeading.style.color = "orange";
+
+const backGround = document.getElementById("header");
+backGround.style.backgroundColor = "green";
+
+const bottomBorder = document.getElementById("header");
+bottomBorder.style.borderBottom = "3px solid orange";
+
+const h2Color = document.getElementById("basket-heading");
+h2Color.style.color = "green";
+
+const addGreetings = document.getElementById("thanks");
+addGreetings.innerHTML = "<p>Please visit us again</p>"
