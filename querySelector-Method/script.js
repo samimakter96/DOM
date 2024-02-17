8b65642c3ebc363d634804523ef1119203fb6d0b
samimakter96 <samimakter9641@gmail.com>
2024-02-17 18:25:00 +0530

    some changes in the querySelector method

--- querySelector-Method/script.js
+++ querySelector-Method/script.js
@@ -1,31 +1,33 @@
-// The following example querySelector finds the first element with the main-heading id:
+const mainHeading = document.querySelector('#basket-heading');
+mainHeading.style.textAlign = 'center';
 
-const mainHeading = document.querySelector("#main-heading");
-mainHeading.style.textAlign = "center";
+const fruits = document.querySelector('.fruits');
+fruits.style.backgroundColor = 'gray';
+fruits.style.padding = '30px';
+fruits.style.margin = '30px';
+fruits.style.width = '50%';
+fruits.style.borderRadius = '5px';
+fruits.style.listStyleType = 'none';
 
-const fruits = document.querySelector(".fruits");
-fruits.style.backgroundColor = "gray";
-fruits.style.padding = "30px";
-fruits.style.margin = "30px";
-fruits.style.width = "50%";
-fruits.style.borderRadius = "5px";
-fruits.style.listStyleType = "none";
+const basketHeading = document.querySelector('#basket-heading');
+basketHeading.style.color = 'brown';
 
 
-// The following example querySelector finds the first element with the basket-heading id:
-
-const basketHeading = document.querySelector("#basket-heading");
-basketHeading.style.color = "green";
-
-// And the following example finds all elements with the fruit pseudo class:
-const OddFruitItems = document.querySelectorAll(".fruit:nth-child(odd)");
-for(let i = 0; i < OddFruitItems.length; i++){
-    OddFruitItems[i].style.backgroundColor = "white";
+const fruitsItems = document.querySelectorAll('.fruit');
+for (let i = 0; i < fruitsItems.length; i++) {
+    fruitsItems[i].style.padding = '10px'
+    fruitsItems[i].style.margin = '10px'
+    fruitsItems[i].style.borderRadius = '5px';
+    // fruitsItems[i].style.backgroundColor = 'white';
 }
 
+const evenItems = document.querySelectorAll('.fruit:nth-child(even)');
+for (let i = 0; i < evenItems.length; i++) {
+    evenItems[i].style.backgroundColor = 'white';
+}
 
-const EvenFruitItems = document.querySelectorAll(".fruit:nth-child(even)");
-for(let i = 0; i < EvenFruitItems.length; i++) {
-    EvenFruitItems[i].style.backgroundColor = "brown";
-    EvenFruitItems[i].style.color = "white";
+const oddItems = document.querySelectorAll('.fruit:nth-child(odd)');
+for (let i = 0; i < oddItems.length; i++) {
+    oddItems[i].style.backgroundColor = 'brown';
+    oddItems[i].style.color = 'white';
 }
