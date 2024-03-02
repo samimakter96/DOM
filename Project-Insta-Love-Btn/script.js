8139be3bc5982c8dbf56a5c0d2f95ac8cd44daf2
samimakter96 <samimakter9641@gmail.com>
2024-03-02 15:41:16 +0530

    instagram love feature

---
+++ Project-Insta-Love-Btn/script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('#container');
+const love = document.querySelector('i');
+
+container.addEventListener('dblclick', function() {
+  love.style.transform = 'translate(-50%, -50%) scale(1)';
+  love.style.opacity = 0.8;
+  setTimeout(function() {
+    love.style.opacity = 0; 
+  }, 1000);
+  setTimeout(function() {
+    love.style.transform = 'translate(-50%, -50%) scale(0)'
+  }, 2000);
+})
